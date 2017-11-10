56995bda95aa24b0145482c732b6badcdb9c7f37
Robbe Van Herck <robbevanherck1@gmail.com>
2017-11-10 14:20:51 +0100

    Translations now using tree structure Verilog language complete Minor regEx changes

--- haskell/generator.hs
+++ haskell/generator.hs
@@ -3,36 +3,82 @@ import Text.Regex.Base
 import Data.Char
 import Data.List
 
+data Tree a = Empty | Node a (Tree a) (Tree a) deriving (Show)
+data Direction = L | R deriving (Show)
+type Directions = [Direction]
+
 main = do
-        putStrLn "Choose language: FPGA, abstract"
+        putStrLn "Choose language: Verilog, abstract"
         language <- getLine
         putStrLn "Input proposition"
         input <- getLine
         let abst = translate input
-        if language == "FPGA"
+        if language == "Verilog"
           then
-            putStrLn (abstrToFPGA abst)
+            putStrLn (abstrToVerilog abst)
           else
             if language == "abstract"
               then
                 putStrLn abst
               else
-                putStrLn "No such language"
+                putStrLn "Language not implemented"
 
-abstrToFPGA :: String -> String
-abstrToFPGA "" = ""
-abstrToFPGA abst = replaceUsingRegex abst opList
-        where allVars = getVariables abst
-              uniqueVars = nub allVars
-              finalVars = [[(var !! (length var - i)) |i <- [1..(length var)]] | var <- uniqueVars, var /= []]
-              varsList = [(mkRegex c, "reg_in[" ++ c ++ "]") | c <- finalVars]
-              andOp = (mkRegex "&", " & ")
-              orOp = (mkRegex "/", " | ")
-              notOp = (mkRegex "!", " ~")
-              impliesOp = (mkRegex ">", " WIP ")
-              equalsOp = (mkRegex "=", " ^~ ")
-              nEqualsOp = (mkRegex "#", " ^ ")
-              opList = andOp:orOp:notOp:impliesOp:equalsOp:nEqualsOp:varsList
+abstrToVerilog :: String -> String
+abstrToVerilog "" = ""
+abstrToVerilog abst = treeToVerilog (strToTree abst)
+
+treeToVerilog :: Tree Char -> String
+treeToVerilog Empty = ""
+treeToVerilog (Node x Empty Empty) = charToVerilog x
+treeToVerilog (Node '>' left right) = "~(" ++ treeToVerilog left ++ ") | " ++ treeToVerilog right
+treeToVerilog (Node x left right) = "(" ++ treeToVerilog left ++ ")" ++ charToVerilog x ++ "(" ++ treeToVerilog right ++ ")"
+
+charToVerilog :: Char -> String
+charToVerilog '&' = " & "
+charToVerilog '/' = " | "
+charToVerilog '!' = " ~ "
+charToVerilog '=' = " ^~ "
+charToVerilog '#' = " ^ "
+charToVerilog '(' = "("
+charToVerilog ')' = ")"
+charToVerilog x = "reg_in[" ++ [x] ++ "]"
+
+-- abstrToVerilog :: String -> String
+-- abstrToVerilog "" = ""
+-- abstrToVerilog abst = replaceUsingRegex (convImplies abst) opList
+--         where allVars = getVariables abst
+--               uniqueVars = nub allVars
+--               finalVars = [[var !! (length var - i) |i <- [1..length var]] | var <- uniqueVars, var /= []]
+--               varsList = [(mkRegex c, "reg_in[" ++ c ++ "]") | c <- finalVars]
+--               andOp = (mkRegex "&", " & ")
+--               orOp = (mkRegex "/", " | ")
+--               notOp = (mkRegex "!", " ~")
+--               equalsOp = (mkRegex "=", " ^~ ")
+--               nEqualsOp = (mkRegex "#", " ^ ")
+--               opList = andOp:orOp:notOp:equalsOp:nEqualsOp:varsList
+
+strToTree :: String -> Tree Char
+strToTree "" = Empty
+strToTree str = Node (str !! sepIdx) (strToTree ll) (strToTree rl)
+        where sepIdx = findSeparator (removeBetweenBrackets str)
+              ll = remOuterBrackets [str !! i | i <- [0..sepIdx-1]]
+              rl = remOuterBrackets [str !! i | i <- [sepIdx+1..length str - 1]]
+
+level :: String -> [Integer]
+level = scanl (\acc c -> acc + (if c=='(' then 1 else if c==')' then -1 else 0)) 0
+
+remOuterBrackets :: String -> String
+remOuterBrackets [] = []
+remOuterBrackets [x] = [x]
+remOuterBrackets str = if minimum (tail (init (level str))) == 0 then str else tail (init str)
+
+removeBetweenBrackets str = [if level str !!i + level str !!(i+1) == 0 then str !! i else 'X'| i <- [0..length str - 1]]
+
+findSeparator :: String -> Int
+findSeparator [] = 0
+findSeparator [x] = 0
+findSeparator [x,y] = 0
+findSeparator str = head [i | j <- "#=>/&!", i <- [0..length str-1], (str !! i) == j]
 
 translate :: String -> String
 translate [] = []
@@ -51,24 +97,24 @@ removeMultiChar str = replaceUsingRegex str [(orReg, " / "), (notReg, " ! "), (a
         where andReg = mkRegexWithOpts "( and |˄)" False True :: Regex
               orReg = mkRegexWithOpts "( or |˅)" False True :: Regex
               impReg = mkRegexWithOpts "( implies |=>|⇒)" False True :: Regex
-              equReg = mkRegexWithOpts "( ((is )*equivalent( with)*)|((is )*equal( to)*))" False True :: Regex
-              nEquReg = mkRegexWithOpts "( ((is )*not equivalent( with)*)|((is )*not equal( to)*))" False True :: Regex
+              equReg = mkRegexWithOpts "( ((is )*equivalent( with)*) | ((is )*equal( to)*) )" False True :: Regex
+              nEquReg = mkRegexWithOpts "( ((is )*not equivalent( with)*) | ((is )*not equal( to)*)) " False True :: Regex
               notReg = mkRegexWithOpts "(^not |( not )|~)" False True :: Regex
 
 replaceUsingRegex :: String -> [(Regex, String)] -> String
 replaceUsingRegex "" _ = ""
 replaceUsingRegex str [] = str
-replaceUsingRegex str opList = foldr (\t -> \accStr -> subRegex (fst t) accStr (snd t)) str opList
+replaceUsingRegex str opList = foldr (\t accStr -> subRegex (fst t) accStr (snd t)) str opList
 
 prepVariables :: String -> String
 prepVariables [] = ""
 prepVariables str = replaceUsingRegex str opList
         where allVars = getVariables str
               uniqueVars = nub allVars
-              finalVars = [[(var !! (length var - i)) |i <- [1..(length var)]] | var <- uniqueVars, var /= []]
-              opList = [((mkRegex (finalVars !! i) :: Regex), [intToDigit i]) | i <- [0..(length finalVars)-1]]
+              finalVars = [[var !! (length var - i) |i <- [1..length var]] | var <- uniqueVars, var /= []]
+              opList = [(mkRegex (finalVars !! i) :: Regex, [intToDigit i]) | i <- [0..length finalVars - 1]]
 
 getVariables :: String -> [String]
-getVariables str = foldl (helper) [[]] str
+getVariables = foldl helper [[]]
         where helper [[]] x = if x `elem` "/!&>=#()" then [[]] else [[x]]
               helper (full@(first:rest)) x = if x `elem` "/!&>=#()" then []:full else (x:first):rest
